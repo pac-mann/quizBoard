81be974469fb581b9be921fc056135ed9215ca77
Kevin Odhiambo Onyango <KOnyango@safaricom.co.ke>
2022-11-27 14:17:22 +0300

    Add log out

--- js/main.js
+++ js/main.js
@@ -2,27 +2,24 @@ var result1 = 1;
 var result2 = 2;
 var result3 = 3;
 
-
-$(document).ready(function() {
-  $("#quiz").submit(function(event) {
-
+$(document).ready(function () {
+  $("#quiz").submit(function (event) {
     event.preventDefault();
 
     var question1 = $("input[name='fav_language1']:checked").val();
     var question2 = $("input[name='fav_language2']:checked").val();
     var question3 = $("input[name='fav_language3']:checked").val();
-   
+
     console.log(question1);
     console.log(question2);
     console.log(question3);
-    
 
-   if (question1 === "right") {
+    if (question1 === "right") {
       result1 = 20;
-    }else {
+    } else {
       result1 = 0;
     }
-    if (question2 ==="right") {
+    if (question2 === "right") {
       result2 = 20;
     } else {
       result2 = 0;
@@ -32,11 +29,18 @@ $(document).ready(function() {
     } else {
       result3 = 0;
     }
-  
-    var score = (result1 + result2 + result3);
+
+    var score = result1 + result2 + result3;
     console.log(score);
 
-    $("#result").text("You have gotten " + score + " points.");
-    
-  })
-})
+    if (score > 50) {
+      $("#result").text("You have passed. Your score is " + score + " points.");
+    } else {
+      $("#result").text("You have failed. Your score is " + score + " points.");
+    }
+  });
+
+  $("#reset").click(function () {
+    $("input[type='radio']").prop("checked", false);
+  });
+});
--- js/script.js
+++ js/script.js
@@ -1,29 +1,20 @@
 //validate email
-$(document).ready(function(){
-    $("#login-form").submit(function(event){
+$(document).ready(function () {
+  $("#login-form").submit(function (event) {
+    event.preventDefault(); //prevent default action of form to quiz.html
 
-       event.preventDefault(); //prevent default action of form to quiz.html
-       
-       var email = $("#email").val();
-       var password = $("#pwd").val();
+    var email = $("#email").val();
+    var password = $("#pwd").val();
 
+    var validRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var validPwdRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
 
-        var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        var validPwdRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-      
-        if (email.match(validRegex) && password.match(validPwdRegex) ){
-      
-          window.location = 'quiz.html'
-    
-        } else {
-      
-          alert("Invalid Password or email address!");
-      
-        }
-      
-
-    })
-
+    if (email.match(validRegex) && password.match(validPwdRegex)) {
+      window.location = "quiz.html";
+    } else {
+      alert("Invalid Password or email address!");
+    }
+  });
 });
-
-
